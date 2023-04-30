eb3dfd2177f6776a4f27f98e4fb8f708d1933e28
offline <8227731+probablyoffline@users.noreply.github.com>
2023-04-30 13:28:38 -0500

    Update reset-brain.sh

--- reset-brain.sh
+++ reset-brain.sh
@@ -2,8 +2,8 @@
 
 echo "[=] Stopping pwnagtochi service"
 sudo systemctl stop pwnagotchi.service
-echo "[+] Erasing past life"
+echo "[-] Erasing past life"
 sudo rm -rf /root/handshakes/ /root/peers/ /var/log/pwnagotchi.log /root/brain.nn /root/brain.json
 echo "[=] Starting pwnagtochi service"
 sudo systemctl start pwnagotchi.service
-echo "[+] Pwnagotchi reset complete"
+echo "[=] Pwnagotchi reset complete"
